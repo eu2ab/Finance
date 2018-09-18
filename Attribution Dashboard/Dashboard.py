ae6ad20a15cf3e8524590a83f3f988a07d1088e1
eu2ab <eu2ab@virginia.edu>
2018-09-18 05:02:02 -0400

    Updates to Dashboard.py

--- Attribution Dashboard/Dashboard.py
+++ Attribution Dashboard/Dashboard.py
@@ -1,4 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from plotly import graph_objs as go
+import datetime as dt
+import json
+from GetData import QUANDL_price
 
+import dash
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_core_components as dcc
+
+#   Build data frame containing positions/information
+df = pd.DataFrame({'Ticker': ['AAPL', 'GOOGL', 'TSLA'],
+                   'Cost Basis': [100, 100, 100],
+                   'Date of Purchase': [dt.date(2016, 1, 1), dt.date(2016, 1, 1), dt.date(2016, 1, 1)],
+                   'Delta': [15, 15, 15]})  # Build initial dataframe
+df['Stock Price'] =
